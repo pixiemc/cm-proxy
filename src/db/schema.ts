9e8e2c2697101188d48e3fe5caf37b24b141c440
xyzeva <xyzeva@riseup.net>
2024-10-30 15:53:33 +0300

    feat(db): add indexes

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,6 @@
 import {
-  bigint,
   boolean,
   index,
-  integer,
   jsonb,
   pgTable,
   text,
@@ -23,28 +21,44 @@ export const users = pgTable(
   })
 );
 
-export const outfits = pgTable("outfits", {
-  id: uuid().notNull().primaryKey().defaultRandom(),
-  ownerId: uuid("owner_id")
-    .notNull()
-    .references(() => users.id),
-  name: text().notNull(),
-  skinTexture: text("skin_texture"),
-  equippedCosmetics: jsonb("equipped_cosmetics"),
-  cosmeticSettings: jsonb("cosmetic_settings"),
-  selected: boolean().notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  favoritedAt: timestamp("favorited_at"),
-  skinId: text("skin_id"),
-});
+export const outfits = pgTable(
+  "outfits",
+  {
+    id: uuid().notNull().primaryKey().defaultRandom(),
+    ownerId: uuid("owner_id")
+      .notNull()
+      .references(() => users.id),
+    name: text().notNull(),
+    skinTexture: text("skin_texture"),
+    equippedCosmetics: jsonb("equipped_cosmetics"),
+    cosmeticSettings: jsonb("cosmetic_settings"),
+    selected: boolean().notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    favoritedAt: timestamp("favorited_at"),
+    skinId: text("skin_id"),
+  },
+  (outfit) => ({
+    idIdx: index("outfits_id_idx").on(outfit.id),
+    ownerIdIdx: index("outfits_owner_id_idx").on(outfit.ownerId),
+    selectedIdx: index("outfits_selected_idx").on(outfit.selected),
+  })
+);
 
-export const emoteWheels = pgTable("emote_wheels", {
-  id: uuid().notNull().primaryKey().defaultRandom(),
-  ownerId: uuid("owner_id")
-    .notNull()
-    .references(() => users.id),
-  selected: boolean().notNull(),
-  slots: jsonb().notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at"),
-});
+export const emoteWheels = pgTable(
+  "emote_wheels",
+  {
+    id: uuid().notNull().primaryKey().defaultRandom(),
+    ownerId: uuid("owner_id")
+      .notNull()
+      .references(() => users.id),
+    selected: boolean().notNull(),
+    slots: jsonb().notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at"),
+  },
+  (emoteWheel) => ({
+    idIdx: index("emote_wheels_id_idx").on(emoteWheel.id),
+    ownerIdIdx: index("emote_wheels_owner_id_idx").on(emoteWheel.ownerId),
+    selectedIdx: index("emote_wheels_selected_idx").on(emoteWheel.selected),
+  })
+);
